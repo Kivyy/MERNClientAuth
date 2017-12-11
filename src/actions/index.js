8a62a030e166195f4c5303ac31cba3acae43ec9c
Kiv Lo <kivlokw@gmail.com>
2017-12-11 14:24:28 -0500

    constructing signin form with redux form

---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+
+
+export function signinUser({email, passowrd}) {
+  //Submit email/pass to server
+
+  // if req is good
+  // - update state to indicate user is authenticated
+  // - save JWT token
+  // - redirect to the feature route
+  
+}
