5c4e242744afd633f26478a750962c60dc69add9
Kiv Lo <kivlokw@gmail.com>
2017-12-12 13:41:01 -0500

    implement signout component

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,36 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router';
 
 class Header extends Component {
+
+  renderLinks(){
+    if(this.props.authenticated) {
+      return <li className='nav-item'> <Link className="nav-link" to='/signout'>Sign Out </Link></li>
+    } else {
+      return [
+      <li className='nav-item'><Link className='nav-link' to='/signin'> Sign in </Link></li>,
+      <li className='nav-item'><Link className="nav-link" to="/signup"> Sign up</Link></li>
+      ]
+    }
+  }
+
   render(){
     return(
       <nav className="navbar navbar-light">
+        <Link to='/' className="navbar-brand"> Redux Auth Boiler </Link>
         <ul className="nav navbar-nav">
-          <li className='nav-item'>
-            Signin
-          </li>
+          {this.renderLinks()}
         </ul>
       </nav>
     );
   }
 }
 
-export default Header;
+function mapStateToProps(state){
+  return {
+    authenticated: state.auth.authenticated
+  }
+}
+
+export default connect(mapStateToProps)(Header);
