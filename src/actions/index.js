85f45b6edbb067a429e74c66c5ffc296c24b6df0
Kiv Lo <kivlokw@gmail.com>
2017-12-18 15:08:10 -0500

    fix signup action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,38 +1,43 @@
 import axios from 'axios';
 import { browserHistory } from 'react-router';
-import { AUTH_USER , AUTH_ERROR , UNAUTH_USER} from './types';
+import {
+  AUTH_USER,
+  UNAUTH_USER,
+  AUTH_ERROR
+} from './types';
 
-const API_URL = 'http://localhost:3000';
+const ROOT_URL = 'http://localhost:3090';
 
-export function signinUser({email, password}) {
-  return function(dispatch){
-    axios.post(`${API_URL}/signin`, {email, password})
-      .then((response) => {
-        dispatch({type: AUTH_USER })
+export function signinUser({ email, password }) {
+  return function(dispatch) {
+    // Submit email/password to the server
+    axios.post(`${ROOT_URL}/signin`, { email, password })
+      .then(response => {
+        // If request is good...
+        // - Update state to indicate user is authenticated
+        dispatch({ type: AUTH_USER });
+        // - Save the JWT token
         localStorage.setItem('token', response.data.token);
-
+        // - redirect to the route '/feature'
         browserHistory.push('/feature');
       })
       .catch(() => {
-        dispatch(authError('Bad Login info'));
+        // If request is bad...
+        // - Show an error to the user
+        dispatch(authError('Bad Login Info'));
       });
   }
 }
 
-export function signupUser({email, password}){
-  return function(dispatch){
-    axios.post(`${API_URL}/signup`, {email, password})
-      .then((response) => {
-        console.log(response)
-        dispatch({type: AUTH_USER })
+export function signupUser({ email, password }) {
+  return function(dispatch) {
+    axios.post(`${ROOT_URL}/signup`, { email, password })
+      .then(response => {
+        dispatch({ type: AUTH_USER });
         localStorage.setItem('token', response.data.token);
-
         browserHistory.push('/feature');
-      });
-      .catch((err) => {
-        console.log('this is err:' , err)
-        dispatch(authError('Email already used'));
-      });
+      })
+      .catch(response => dispatch(authError('Email already registered.')));
   }
 }
 
@@ -46,5 +51,5 @@ export function authError(error) {
 export function signoutUser() {
   localStorage.removeItem('token');
 
-  return { type: UNAUTH_USER}
+  return { type: UNAUTH_USER };
 }
